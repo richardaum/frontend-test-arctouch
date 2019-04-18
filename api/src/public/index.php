516abefc68ee398002a2ab804cc8ec1090215f3f
Richard Lopes <richard.lopes92@gmail.com>
2019-04-18 01:14:22 -0300

    Enabling cors on api

--- api/src/public/index.php
+++ api/src/public/index.php
@@ -7,6 +7,17 @@ require "../endpoints/movie-search/index.php";
 require "../infrastructure/tmdb/TmdbApi.php";
 
 $app = new \Slim\App;
+
+// CORS
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+// API endpoints
 $app->group("/api", function () {
     $this->get("/movies", "movies");
     $this->get("/movie/search", "movieSearch");
