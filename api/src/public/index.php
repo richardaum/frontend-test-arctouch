af5d13b95678cad0a02529fb6ed43ad3a8aaddab
Richard Lopes <richard.lopes92@gmail.com>
2019-04-18 22:29:10 -0300

    Creating geners api endpoint

--- api/src/public/index.php
+++ api/src/public/index.php
@@ -4,6 +4,7 @@ require "../infrastructure/dotenv.php";
 require "../endpoints/movies/index.php";
 require "../endpoints/movie/index.php";
 require "../endpoints/movie-search/index.php";
+require "../endpoints/movie-genres/index.php";
 require "../infrastructure/tmdb/TmdbApi.php";
 
 $app = new \Slim\App;
@@ -21,6 +22,7 @@ $app->add(function ($req, $res, $next) {
 $app->group("/api", function () {
     $this->get("/movies", "movies");
     $this->get("/movie/search", "movieSearch");
+    $this->get("/movie/genres", "movieGenres");
     $this->get("/movie/{id}", "movieDetails");
 });
 $app->run();
